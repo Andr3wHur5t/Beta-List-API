9955a29726458addb36c763dad55c519a3b62f08
GalacticCow <MattJTrueblood@gmail.com>
2014-06-24 10:26:54 -0700

    Created base server with illegal character detection

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 /**
  * Created by matthew on 6/23/14.
  */
+var server = require('http').createServer(function (request, response) {
+    var uri = request.url.toString();
+    response.writeHead(200, {"Content-Type": "text/plain"});
+
+    if(uri.indexOf(';') != -1 || uri.indexOf(')') != -1){
+        console.log("; or ) character detected!!!");
+        response.end("Error 403: address contains illegal characters");
+    }
+
+
+    response.end("Successfully added email address (well not really, but I swear it will soon!.")
+});
+
+server.listen(8080);
+console.log("Server running at http://127.0.0.1:8000/");
